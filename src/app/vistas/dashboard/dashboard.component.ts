09c436918ccf02f852c28b744fa26e12fed7f221
xiyneb <jassin.2498@gmail.com>
2024-08-04 19:37:25 -0400

    act

--- src/app/vistas/dashboard/dashboard.component.ts
+++ src/app/vistas/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit{
+  nombre: string = '';
+  
+  ngOnInit(): void {
+    let fecha = formatDate(new Date(), 'HH', 'en-US')
+    if (Number(fecha) < 12) {
+      this.nombre = 'Buenos dias '+ localStorage.getItem('nombre');
+    }else
+    if (Number(fecha) >= 12 && Number(fecha) < 18) {
+      this.nombre = 'Buenas tardes '+ localStorage.getItem('nombre');
+    }else
+    if (Number(fecha) >= 18) {
+      this.nombre = 'Buenas noches '+ localStorage.getItem('nombre');
+    }
+  }
 }
--- src/app/vistas/repventas/repventas.component.ts
+++ src/app/vistas/repventas/repventas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VentasService } from 'src/app/service/ventas/ventas.service';
 
 @Component({
   selector: 'app-repventas',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./repventas.component.css']
 })
 export class RepventasComponent {
+  fecha: string = '';
+  usuario: string = '';
+  totalventa: string = '';
+  montototal: string = '';
+  sabores: string = '';
+  list: any[] = [];
 
+  constructor(private api:VentasService){
+
+  }
+
+  mostrar(){
+    this.api.getreport({ fecha: this.fecha }).subscribe(data => {
+      this.totalventa = data.result.totalventa;
+      this.montototal = data.result.montototal;
+      this.sabores = data.result.sabores;
+      this.usuario = data.message;
+      this.list = data.detalle;
+      console.log(data);
+    })
+  }
 }
